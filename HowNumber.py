224ad129b8b67dc549fccc7756aa931b016a8c47
Amro_Abdulrahim <ammory745@gmail.com>
2024-12-05 09:05:42 +0300

    calculation game

---
+++ HowNumber.py
@@ -0,0 +1,29 @@
+import random
+
+print("All You Need is to make calculations in your mind")
+print("Press Enter to Continue")
+continue1 = input("...")
+
+num = random.randint(2, 100)
+
+print("Select a random number, but do not tell me")
+
+continue1 = input("...")
+
+print("The brother gave you the same number (Collect them)")
+
+continue1 = input("...")
+
+print(f"I give you {num}")
+
+continue1 = input("...")
+
+print("Throw half of them in the sea")
+
+continue1 = input("...")
+
+print("Refer to yor brother his money")
+
+continue1 = input("...")
+
+print(f"The rest for you is: {num / 2}")
